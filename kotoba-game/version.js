5919be7c43024c87b5468c376a62a1e1a0fa1115
Naoki Hosoya <hosoya.naoki@moneyforward.co.jp>
2026-08-16 18:45:25 +0900

    Bump game version to 2026.08.16.4

--- kotoba-game/version.js
+++ kotoba-game/version.js
@@ -1,5 +1,5 @@
 (()=>{
-  const APP_VERSION = '2026.08.16.3';
+  const APP_VERSION = '2026.08.16.4';
   const start = document.getElementById('start');
   if (!start || document.getElementById('appVersion')) return;
 
