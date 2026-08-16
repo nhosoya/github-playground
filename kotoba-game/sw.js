4c5fa2ce45f145dfa05cb74e91ca08b550b20f75
Naoki Hosoya <hosoya.naoki@moneyforward.co.jp>
2026-08-16 18:29:42 +0900

    Add service worker for word game PWA

---
+++ kotoba-game/sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'kotoba-game-v1';
+const CORE = ['./', './index.html', './record.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match(request).then(hit => hit || caches.match('./')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then(hit => hit || fetch(request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(request, copy));
+      return response;
+    }))
+  );
+});
