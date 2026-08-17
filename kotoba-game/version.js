239ab280f05cd1f65204a8da1283c75f44b00a6c
Naoki Hosoya <hosoya.naoki@moneyforward.co.jp>
2026-08-17 15:50:53 +0900

    Bump game version for preset artwork

--- kotoba-game/version.js
+++ kotoba-game/version.js
@@ -1,5 +1,5 @@
 (()=>{
-  const APP_VERSION = '2026.08.16.18';
+  const APP_VERSION = '2026.08.16.19';
   const start = document.getElementById('start');
   if (!start || document.getElementById('appVersion')) return;
 
